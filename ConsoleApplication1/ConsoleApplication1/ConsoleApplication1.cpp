b1b8ba25a8184ce792ff238ced7dcb3a228426b3
o0serena0o <57519051+o0serena0o@users.noreply.github.com>
2020-02-01 19:53:29 +0900

    71번 문제

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+void plus_print(int n, int m, int i)
+{
+	n = n + (i - 1);
+	if (n > m) {
+		return;
+	}
+	if (i % 2 == 0) {
+		if (n % 2 != 0) {
+			printf("%d ", n);
+		}
+	}
+	if (i % 2 != 0) {
+		if (n % 2 == 0) {
+			printf("%d ", n);
+		}
+	}
+	plus_print(n,m,++i);
+}
+
+int main()
+{
+	int n = 0, m = 0;
+	scanf_s("%d %d", &n, &m);
+	plus_print(n, m,1);
+}
