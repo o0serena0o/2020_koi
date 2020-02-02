f0a6009b9b1feee3bfcdbf3338c31d17fc0d94a0
o0serena0o <57519051+o0serena0o@users.noreply.github.com>
2020-02-02 14:05:31 +0900

    미완료

---
+++ 78/78/78.cpp
@@ -0,0 +1,38 @@
+﻿#pragma warning (disable:4996)
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+int main()
+{
+	int n = 0;
+	int a[20];
+	int min_b = 0;
+	int min_a = 0;
+	int o = 0;
+
+	scanf_s("%d", &n);
+	
+	for (int i = 0; i < n; i++)
+	{
+		scanf_s("%d", &a[i]);
+	}
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		for (int j = 0; j < n - 1 - i; j++)
+		{
+			if (a[j] < a[j + 1])
+			{
+				o = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = o;
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d ", a[i]);
+	}
+}
