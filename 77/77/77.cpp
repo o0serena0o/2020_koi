b663b9770fe3801f355afd1c7a5b78addf4b886d
o0serena0o <57519051+o0serena0o@users.noreply.github.com>
2020-02-02 12:31:22 +0900

    77번 #pragma warning (disable:4996) //맨 위에

---
+++ 77/77/77.cpp
@@ -0,0 +1,39 @@
+﻿#pragma warning (disable:4996)
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+int main()
+{
+	int n = 0;
+	int a[20];
+    int o = 0;
+    int pls = 0;
+
+	scanf("%d", &n);
+	
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &a[i]);
+        pls = pls + a[i];
+	}
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (a[j] < a[j + 1])
+            {
+                o = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = o;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    printf("\n%d", pls);
+}
