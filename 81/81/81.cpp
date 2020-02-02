87d40c5a801db9ff79c62558fd33d8867dfc9eb2
o0serena0o <57519051+o0serena0o@users.noreply.github.com>
2020-02-02 16:35:43 +0900

    81번

---
+++ 81/81/81.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+int main()
+{
+	int n = 0;
+	int max = -1000, min = 1000;
+	int o = 0;
+
+	scanf_s("%d", &n);
+
+	for (int i = 0; i < n; i++)
+	{
+		scanf_s("%d", &o);
+		if (o > max) {
+			max = o;
+		}
+		if (o < min) {
+			min = o;
+		}
+	}
+	printf("\n최댓값 : %d 최솟값 : %d 합 : %d", max, min, max + min);
+}
