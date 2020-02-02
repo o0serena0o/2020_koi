16ccdd71a2935d703aa9ac171b1fb9b1ffa61df9
o0serena0o <57519051+o0serena0o@users.noreply.github.com>
2020-02-02 12:22:09 +0900

    76번

---
+++ 76/76/76.cpp
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+int main()
+{
+	int n = 0;
+	int a[20];
+    int o = 0;
+    int cnt = 0;
+	
+    scanf_s("%d", &n);
+	
+    for (int i = 0; i < n; i++)
+	{
+		scanf_s("%d", &a[i]);
+	}
+    
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            cnt++;
+            if (a[j] > a[j + 1])
+            {
+                o = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = o;
+                cnt++;
+            }
+        }
+    }
+    
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    
+    printf("\n%d", cnt);
+}
