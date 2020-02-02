ad11a1234a64a0b7d3c17c77cf27d68899f463c0
o0serena0o <57519051+o0serena0o@users.noreply.github.com>
2020-02-02 16:28:24 +0900

    80번

---
+++ 80/80/80.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+int main()
+{
+	int a = 0, b = 0;
+	int k = 0, j = 0;
+
+	scanf_s("%d %d", &a, &b);
+	for (int i = a; i <= b; i++)
+	{
+		if (i % 2 == 0)
+		{
+			k = k + i;
+		}
+		if (i % 2 != 0)
+		{
+			j = j + i;
+		}
+	}
+	printf("짝수합 : %d 홀수합 : %d", k, j);
+}
