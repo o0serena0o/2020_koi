25b1c7fe8b68e0100daf1fed5dba0619d6437008
o0serena0o <57519051+o0serena0o@users.noreply.github.com>
2020-02-08 20:15:42 +0900

    93 a[i]>=max max=i;

---
+++ 93/93/93.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+int a[10];
+
+int main()
+{
+	long jam = 0;
+	int non = 0;
+
+	scanf_s("%d", &jam);
+
+
+	while (jam != 0)
+	{
+		int o = jam % 10;
+		a[o] = a[o] + 1;
+		jam = jam / 10;
+	}
+
+	for (int i = 1; i < 10; i++)
+	{
+		if (a[i] >= non)
+		{
+			non = i;
+		}
+	}
+	printf("%d", non);
+}
