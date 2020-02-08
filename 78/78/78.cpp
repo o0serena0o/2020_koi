db65fa36aeb185315fde8b4a4e8eae5700239e38
o0serena0o <57519051+o0serena0o@users.noreply.github.com>
2020-02-08 18:41:19 +0900

    for(int i=0~n-1) if(a[i]<min) { min=a[i]; idx=i; }

--- 78/78/78.cpp
+++ 78/78/78.cpp
@@ -10,12 +10,18 @@ int main()
 	int min_b = 0;
 	int min_a = 0;
 	int o = 0;
+	int min = 10000;
 
 	scanf_s("%d", &n);
 	
 	for (int i = 0; i < n; i++)
 	{
 		scanf_s("%d", &a[i]);
+		if (a[i] < min)
+		{
+			min = a[i];
+			min_b = i;
+		}
 	}
 
 	for (int i = 0; i < n - 1; i++)
@@ -31,8 +37,10 @@ int main()
 		}
 	}
 
+	min_a = n - 1;
 	for (int i = 0; i < n; i++)
 	{
 		printf("%d ", a[i]);
 	}
+	printf("\n%d", min_a - min_b);
 }
