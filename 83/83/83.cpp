2c0ce3c5c3727b46a2dbcda6a5656b6fb59a0167
o0serena0o <57519051+o0serena0o@users.noreply.github.com>
2020-02-08 19:18:48 +0900

    83

---
+++ 83/83/83.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+int main()
+{
+	long jam = 0;
+	int k = 0;
+	int cnt = 0;
+
+	scanf_s("%d", &jam);
+	scanf_s("%d", &k);
+
+
+	while (jam / 10 != 0)
+	{
+		if (jam % 10 == k)
+		{
+			cnt++;
+			jam = jam / 10;
+		}
+		else
+		{
+			jam = jam / 10;
+		}
+	}
+	printf("%d", cnt);
+}
