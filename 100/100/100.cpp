33978a13c30c357550d55c36e57e9a3d88a5be1d
o0serena0o <57519051+o0serena0o@users.noreply.github.com>
2020-02-15 20:30:40 +0900

    백준온라인저지 14867번 단순 재귀호출 구현 두번째 테스트케이스 최소횟수 반환 안됨.

---
+++ 100/100/100.cpp
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+int map[5][5] = {
+	{1,3,2,4,1},
+	{5,6,8,5,0},
+	{6,7,4,2,4},
+	{5,4,5,3,2},
+	{5,4,0,0,9}
+};
+
+int cross_number = 0;
+
+void cn(int x, int y)
+{
+	cross_number = map[x][y - 1] + map[x][y + 1] + map[x - 1][y] + map[x + 1][y] + map[x][y];
+	printf("(%d,%d) 십자수는 %d입니다", x, y, cross_number);
+}
+
+int main()
+{
+	int k = 0;
+	int x = 0, y = 0;
+	scanf_s("%d", &k);
+	for (int i = 0; i < k; i++)
+	{
+		scanf_s("%d %d", &x, &y);
+		cn(x, y);
+		x = 0, y = 0;
+	}
+}
---
+++ 14867/14867/14867.cpp
@@ -0,0 +1,61 @@
+﻿#include<iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int a, b, A, B;
+int x, y;
+int check[1000][1000];
+
+void solve(int a, int b, int cnt)
+{
+	if (a == x && b == y)
+	{
+		printf("%d", cnt);
+		return;
+	}
+	printf("%d %d %d", a, b, cnt);
+	if (check[A][b] == 0)
+	{
+		check[A][b] = 1;
+		solve(A, b, cnt+1);
+	}
+	if (check[a][B] == 0)
+	{
+		check[a][B] = 1;
+		solve(a, B, cnt+1);
+	}
+	if (check[0][b] == 0)
+	{
+		check[0][b] = 1;
+		solve(0, b, cnt+1);
+	}
+	if (check[a][0] == 0)
+	{
+		check[a][0] = 1;
+		solve(a, 0, cnt+1);
+	}
+	if (check[0][a + b] == 0)
+	{
+		if (a < B - b)
+		{
+			check[0][a + b] = 1;
+			solve(0, a + b, cnt+1);
+		}
+	}
+	if (check[a - (B - b)][B] == 0)
+	{
+		if (a > B - b)
+		{
+			check[a - (B - b)][B] = 1;
+			solve(a - (B - b), B, cnt+1);
+		}
+	}
+}
+
+int main()
+{
+	scanf_s("%d %d %d %d", &A, &B, &x, &y);
+	solve(0, 0, 0);
+}
