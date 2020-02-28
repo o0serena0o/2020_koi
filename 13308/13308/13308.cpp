5595436a3c64e1b7401fb117b169993cb7250d21
o0serena0o <57519051+o0serena0o@users.noreply.github.com>
2020-02-28 15:51:38 +0900

    13308 dp풀이

--- 13308/13308/13308.cpp
+++ 13308/13308/13308.cpp
@@ -1,20 +1,46 @@
-﻿// 13308.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <bits/stdc++.h>
+using namespace std;
 
-#include <iostream>
+typedef long long ll;
+typedef pair<int, int> p;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+struct asdf {
+	ll node, cost, dst;
+	asdf(int a, int b, ll c) : node(a), cost(b), dst(c) {}
+	bool operator < (asdf r) const { return dst > r.dst; }
+};
+
+bool chk[3000][3000];
+ll n, m, node[3000];
+vector<p> g[3000];
+
+
+ll dijkstra() {
+	priority_queue<asdf> pq;
+	pq.push(asdf(1, node[1], 0));
+	while (!pq.empty()) {
+		asdf now = pq.top();
+		ll idx = now.node, cost = now.cost, dist = now.dst;
+		pq.pop();
+		if (chk[idx][cost]) continue;
+		if (idx == n) return dist;
+
+		chk[idx][cost] = 1;
+		for (auto nxt : g[idx]) {
+			pq.push(asdf(nxt.first, min(cost, node[nxt.first]), cost * nxt.second + dist));
+		}
+	}
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+int main() {
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) cin >> node[i];
+	for (int i = 0; i < m; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		g[a].push_back({ b, c });
+		g[b].push_back({ a, c });
+	}
 
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+	cout << dijkstra();
+}
---
+++ 13308dp/13308dp/13308dp.cpp
@@ -0,0 +1,54 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+const int n_ = 2500 + 10;
+
+struct node
+{
+	int idx, cst;
+	ll dst;
+	node(int idx, int cst, ll dst) :idx(idx), cst(cst), dst(dst) {}
+	bool operator < (node A)const { return dst > A.dst; }
+};
+
+bool vst[n_][n_];
+int n, m, cst[n_];
+vector<pii> gph[n_];
+priority_queue<node> pq;
+
+int main()
+{
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) cin >> cst[i];
+	for (int i = 0; i < m; i++)
+	{
+		int a, b, c;
+		cin >> a >> b >> c;
+		gph[a].push_back({ b,c });
+		gph[b].push_back({ a,c });
+	}
+
+	pq.push(node(1, cst[1], 0));
+	while (!pq.empty())
+	{
+		node now = pq.top();
+		pq.pop();
+		if (vst[now.idx][now.cst])continue;
+		if (now.idx == n)
+		{
+			cout << now.dst;
+			break;
+		}
+		vst[now.idx][now.cst] = true;
+		for (auto nxt : gph[now.idx])
+		{
+			pq.push(node(nxt.first, min(now.cst, cst[nxt.first]), now.cst * nxt.second + now.dst));
+		}
+	}
+
+	return 0;
+}
