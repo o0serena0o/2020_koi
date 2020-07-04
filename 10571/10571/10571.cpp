985290d94af590651b4d38857f74f76a0e22ad1f
o0serena0o <57519051+o0serena0o@users.noreply.github.com>
2020-07-04 15:16:49 +0900

    7/4

---
+++ 10571/10571/10571.cpp
@@ -0,0 +1,43 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int t;
+int n;
+vector<pair<int, int>> d;
+int w, c;
+vector<int>dp;
+int diamond;
+int max_dp;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> t >> n;
+	while (t--)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			cin >> w >> c;
+			d.push_back(make_pair(w, c));
+		}
+		for (int i = 0; i < n; i++)
+		{
+			dp[i] = 1;
+			for (int j = 0; j < i; j++)
+			{
+				if (d[j].first<d[i].first && d[j].second>d[i].second)
+				{
+					dp[i] = max(dp[j] + 1, dp[i]);
+				}
+			}
+		}
+		int ans = dp[0];
+		for (int i = 0; i < n; i++)
+		{
+			ans = max(ans, dp[i]);
+		}
+		cout << ans;
+	}
+}
--- 2565/2565/2565.cpp
+++ 2565/2565/2565.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int elec;
 vector<int> dp;
 vector<pair<int,int>> a;
+int max_dp = 0;
+int lis;
 
 int main()
 {
@@ -18,9 +20,22 @@ int main()
 		cin >> r >> k;
 		a.push_back(make_pair(r, k));
 	}
-	sort(a.begin, a.end);
+	sort(a.begin(), a.end());
 	for (int i = 0; i < elec; i++)
 	{
-		a[i].second
+		max_dp = 0;
+		for (int j = 0; j < i; j++)
+		{
+			if (a[j].second<a[i].second && dp[j]>max_dp)
+			{
+				max_dp = dp[j];
+			}
+		}
+		dp.push_back(max_dp+1);
 	}
+	for (int i = a.size()-1; i >= 0; i--)
+	{
+		if (lis < dp[i]) lis = dp[i];
+	}
+	cout << elec-lis;
 }
